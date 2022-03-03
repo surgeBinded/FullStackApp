845510d1ff0f59c520703bba9f8dec2864c16861
surgeBinded <ropotdaniel@gmail.com>
2022-03-03 12:51:56 +0100

    add endpoint for adding employees

--- src/main/kotlin/com/daro/fullstackapp/controller/EmployeeController.kt
+++ src/main/kotlin/com/daro/fullstackapp/controller/EmployeeController.kt
@@ -3,11 +3,9 @@ package com.daro.fullstackapp.controller
 import com.daro.fullstackapp.model.Employee
 import com.daro.fullstackapp.repository.EmployeeRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["http://localhost:3000/"])
 @RestController
 @RequestMapping("/api/v1")
 class EmployeeController @Autowired constructor(private val employeeRepository: EmployeeRepository) {
@@ -17,4 +15,8 @@ class EmployeeController @Autowired constructor(private val employeeRepository: 
 
     @GetMapping("/employees/{employeeId}")
     fun getEmployee(@PathVariable employeeId: Long): Employee = employeeRepository.findById(employeeId).get()
+
+    @PostMapping("/employees")
+    fun createEmployee(@RequestBody employee: Employee) = employeeRepository.save(employee)
+
 }
